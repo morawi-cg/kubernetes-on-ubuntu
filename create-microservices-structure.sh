20fee2936bfb49a069d6c84db33a0aff803a9bd7
engineer <engineer@mazinger.techw6.local>
2020-02-20 22:40:02 +0000

    Added the main file to be put into userdate section for the AWS instance

---
+++ create-microservices-structure.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#!/bin/bash
+
+# Docker ce time 19.
+
+sudo apt -y remove docker docker-engine docker.io
+sudo apt -y update
+sudo apt install -y apt-transport-https software-properties-common ca-certificates curl wget
+wget https://download.docker.com/linux/ubuntu/gpg
+sudo apt-key add gpg
+sudo apt -y update
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-cache policy docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
+
+##Kubernetes time
+#Must do swap off else kube will not perform well
+sudo swapoff -a
+
+# ensure legacy binaries are installed
+sudo apt-get install -y iptables arptables ebtables
+
+# switch to legacy versions
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
+sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+sudo apt-get -y update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get -y update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo systemctl daemon-reload
+sudo systemctl restart kubelet
+
+# How to join
+
+# sudo kubeadm join 192.168.100.6:6443 --token 06tl4c.oqn35jzecidg0r0m --discovery-token-ca-cert-hash sha256:c40f5fa0aba6ba311efcdb0e8cb637ae0eb8ce27b7a03d47be6d966142f2204c
+
+exit 0
