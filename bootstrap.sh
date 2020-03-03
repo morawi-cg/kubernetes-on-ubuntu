fcf50e8d9bc9056de7d06ca3ac3aa7aac16f803a
Software Engineer, Virtualization specialist <morawi-cg@users.noreply.github.com>
2020-03-03 20:53:44 +0000

    Update bootstrap.sh
    JDK added

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,5 +28,51 @@ sudo apt -y install ansible
 sudo apt -y install git
 git clone --recursive https://github.com/kubernetes-sigs/kubespray.git
 
+#### Must have JDK8 copied to this folder, be careful as this is Oracle version used for Home/Education usage only
+
+#!/bin/bash
+/usr/bin/update-alternatives --install jdk1.8.0_241/bin/java java /bin/java 1
+/usr/bin/update-alternatives --install /home/vagrant/jdk1.8.0_241/bin/java java /bin/java 1
+/usr/bin/update-alternatives --install /bin/java java /home/vagrant/jdk1.8.0_241/bin/java 1
+/usr/bin/update-alternatives --install /bin/javac javac /home/vagrant/jdk1.8.0_241/bin/javac 1
+/usr/bin/update-alternatives --install /bin/javaws javaws /home/vagrant/jdk1.8.0_241/bin/javaws 1
+/usr/bin/update-alternatives --install /bin/jar jar /home/vagrant/jdk1.8.0_241/bin/jar 1
+/usr/bin/update-alternatives --install /bin/javadoc javadoc /home/vagrant/jdk1.8.0_241/bin/javadoc 1
+/usr/bin/update-alternatives --install /bin/javah javah /home/vagrant/jdk1.8.0_241/bin/javah 1
+/usr/bin/update-alternatives --install /bin/jconsole jconsole /home/vagrant/jdk1.8.0_241/bin/jconsole 1
+/usr/bin/update-alternatives --install /bin/jdb jdb /home/vagrant/jdk1.8.0_241/bin/jdb 1
+/usr/bin/update-alternatives --install /bin/jardesigner jardesigner /home/vagrant/jdk1.8.0_241/bin/jardesigner 1
+/usr/bin/update-alternatives --install /bin/jarsigner jarsigner /home/vagrant/jdk1.8.0_241/bin/jarsigner 1
+/usr/bin/update-alternatives --install /bin/javap javap /home/vagrant/jdk1.8.0_241/bin/javap 1
+/usr/bin/update-alternatives --install /bin/jcmd jcmd /home/vagrant/jdk1.8.0_241/bin/jcmd 1
+/usr/bin/update-alternatives --install /bin/tools tools /home/vagrant/jdk1.8.0_241/lib/tools.jar 1
+/usr/bin/update-alternatives --install /bin/jps jps /home/vagrant/jdk1.8.0_241/bin/jps 1
+/usr/bin/update-alternatives --install /bin/jrunscript jrunscript /home/vagrant/jdk1.8.0_241/bin/jrunscript 1
+/usr/bin/update-alternatives --install /bin/jvisualvm jvisualvm /home/vagrant/jdk1.8.0_241/bin/jvisualvm 1
+/usr/bin/update-alternatives --install /bin/jstack jstack /home/vagrant/jdk1.8.0_241/bin/jstack 1
+/usr/bin/update-alternatives --install /bin/jstat jstat /home/vagrant/jdk1.8.0_241/bin/jstat 1
+/usr/bin/update-alternatives --install /bin/jsadebugd jsadebugd /home/vagrant/jdk1.8.0_241/bin/jsadebugd 1
+/usr/bin/update-alternatives --install /bin/jrunscript jrunscript /home/vagrant/jdk1.8.0_241/bin/jrunscript 1
+/usr/bin/update-alternatives --install /bin/jjs jjs /home/vagrant/jdk1.8.0_241/bin/jjs 1
+/usr/bin/update-alternatives --install /bin/jhat jhat /home/vagrant/jdk1.8.0_241/bin/jhat 1
+/usr/bin/update-alternatives --install /bin/jmc jmc /home/vagrant/jdk1.8.0_241/bin/jmc 1
+/usr/bin/update-alternatives --install /bin/jinfo jinfo /home/vagrant/jdk1.8.0_241/bin/jinfo 1
+/usr/bin/update-alternatives --install /bin/jmap jmap /home/vagrant/jdk1.8.0_241/bin/jmap 1
+/usr/bin/update-alternatives --install /bin/jcontrol jcontrol /home/vagrant/jdk1.8.0_241/bin/jcontrol 1
+/usr/bin/update-alternatives --install /bin/jdb jdb /home/vagrant/jdk1.8.0_241/bin/jdb 1
+/usr/bin/update-alternatives --install /bin/jdeps jdeps /home/vagrant/jdk1.8.0_241/bin/jdeps 1
+/usr/bin/update-alternatives --install /bin/javapackager javapackager /home/vagrant/jdk1.8.0_241/bin/javapackager 1
+/usr/bin/update-alternatives --install /bin/java-rmi.cgi java-rmi.cgi /home/vagrant/jdk1.8.0_241/bin/java-rmi.cgi 1
+/usr/bin/update-alternatives --install /bin/jcmd jcmd /home/vagrant/jdk1.8.0_241/bin/ 1
+/usr/bin/update-alternatives --install /bin/idlj idlj /home/vagrant/jdk1.8.0_241/bin/idlj 1
+/usr/bin/update-alternatives --install /bin/javafxpackager javafxpackager /home/vagrant/jdk1.8.0_241/bin/javafxpackager 1
+/usr/bin/update-alternatives --install /bin/appletviewer appletviewer /home/vagrant/jdk1.8.0_241/bin/appletviewer 1
+/usr/bin/update-alternatives --install /bin/ControlPanel ControlPanel /home/vagrant/jdk1.8.0_241/bin/ControlPanel 1
+/usr/bin/update-alternatives --install /bin/extcheck extcheck /home/vagrant/jdk1.8.0_241/bin/extcheck 1
+
+exit 0
+
+
+
 exit 0
 
